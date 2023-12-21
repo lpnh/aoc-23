a6acb11a193607be2139b2c72c7f52c7bb3486d1
lpnh <lipe.paniguel@gmail.com>
2023-12-21 07:55:35 -0300

    refactor code

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -14,20 +14,14 @@ fn solve_part_1(input: &str) -> Result<String, Error> {
         let start = line.find(':').ok_or(anyhow!("':' not found"))? + 1;
         let end = line.find('|').ok_or(anyhow!("'|' not found"))?;
 
-        let winning_numbers: Vec<i32> = line[start..end]
-            .split_whitespace()
-            .map(|s| s.parse::<i32>())
-            .collect::<Result<Vec<i32>, _>>()
-            .map_err(|e| anyhow!("Error parsing numbers: {}", e))?;
+        let winning_numbers = &line[start..end];
+        let winning_numbers_array = get_array(winning_numbers)?;        
 
-        let elf_numbers = line[(end + 1)..]
-            .split_whitespace()
-            .map(|s| s.parse::<i32>())
-            .collect::<Result<Vec<i32>, _>>()
-            .map_err(|e| anyhow!("Error parsing numbers: {}", e))?;
+        let elf_numbers = &line[(end + 1)..];
+        let elf_numbers_array = get_array(elf_numbers)?;
 
-        for number in elf_numbers {
-            if winning_numbers.contains(&number) {
+        for number in elf_numbers_array {
+            if winning_numbers_array.contains(&number) {
                 my_winning_numbers += 1;
             }
         }
@@ -44,6 +38,13 @@ fn solve_part_1(input: &str) -> Result<String, Error> {
 
 fn solve_part_2(input: &str) -> Result<String, Error> {
     good_luck!(input)
+}
+
+fn get_array(numbers: &str) -> Result<Vec<i32>, Error> {
+    numbers.split_whitespace()
+        .map(|s| s.parse::<i32>())
+        .collect::<Result<Vec<i32>, _>>()
+        .map_err(|e| anyhow!("Error parsing numbers: {}", e))
 }
 
 fn main() -> Result<(), Error> {
@@ -75,10 +76,15 @@ mod tests {
     #[test]
     fn check_part_2() {
         const EXAMPLE_2: &str = "
-            example input part 2 goes here
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
         ";
 
-        const EXPECTED_ANSWER_2: &str = "";
+        const EXPECTED_ANSWER_2: &str = "30";
 
         test_part_two!();
     }

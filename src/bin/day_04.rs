6874b11f7ffd19ce76dd4e17d7db7912e854e8ec
lpnh <lipe.paniguel@gmail.com>
2023-12-21 04:08:39 -0300

    add Day04 part_1 solution

---
+++ src/bin/day_04.rs
@@ -0,0 +1,85 @@
+use aoc_23::*;
+
+use anyhow::{Error, Result, anyhow};
+
+const CURRENT_DAY: Day = Day::Day04;
+const PUZZLE_INPUT: &str = include_str!("../../puzzle_input/day_04.txt");
+
+fn solve_part_1(input: &str) -> Result<String, Error> {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut my_winning_numbers = 0;
+
+        let start = line.find(':').ok_or(anyhow!("':' not found"))? + 1;
+        let end = line.find('|').ok_or(anyhow!("'|' not found"))?;
+
+        let winning_numbers: Vec<i32> = line[start..end]
+            .split_whitespace()
+            .map(|s| s.parse::<i32>())
+            .collect::<Result<Vec<i32>, _>>()
+            .map_err(|e| anyhow!("Error parsing numbers: {}", e))?;
+
+        let elf_numbers = line[(end + 1)..]
+            .split_whitespace()
+            .map(|s| s.parse::<i32>())
+            .collect::<Result<Vec<i32>, _>>()
+            .map_err(|e| anyhow!("Error parsing numbers: {}", e))?;
+
+        for number in elf_numbers {
+            if winning_numbers.contains(&number) {
+                my_winning_numbers += 1;
+            }
+        }
+
+        sum += match my_winning_numbers {
+            1 => 1,
+            n if n > 1 => 2_i32.pow(n - 1),
+            _ => 0,
+        };
+    }
+
+    Ok(sum.to_string())
+}
+
+fn solve_part_2(input: &str) -> Result<String, Error> {
+    good_luck!(input)
+}
+
+fn main() -> Result<(), Error> {
+    Advent::ho_ho_ho(elf_magic!())?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn check_part_1() {
+        const EXAMPLE_1: &str = "
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        ";
+
+        const EXPECTED_ANSWER_1: &str = "13";
+
+        test_part_one!();
+    }
+
+    #[test]
+    fn check_part_2() {
+        const EXAMPLE_2: &str = "
+            example input part 2 goes here
+        ";
+
+        const EXPECTED_ANSWER_2: &str = "";
+
+        test_part_two!();
+    }
+}
